891181859b8e8c62bf4e8e69de88a633db5a26c8
andrew <ayietaphilip00@outlook.com>
2020-07-17 17:47:19 +0300

    Add a function thats going to requist the user for the date

--- js/script.js
+++ js/script.js
@@ -1,2 +1,9 @@
 var male = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]
 var female =["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]
+
+
+function myFunction() {
+    var x = document.getElementById("myDate").value
+    document.getElementById("demo").innerHTML = x
+
+}
