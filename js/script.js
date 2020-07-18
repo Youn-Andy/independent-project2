015e579d8f1f3af82715cb0d41c3a435e1f11554
andrew <ayietaphilip00@outlook.com>
2020-07-18 12:46:58 +0300

    Add a  bootstrap style sheet link  to the index html file

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 var male = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]
-var female =["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]
-var d = function(cc, yy, mm, dd){
-    return (((cc/4)-2*cc-1)+((5*yy/4))+((26*(mm+1)/10))+dd)%7
+var female = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]
+var d = function (cc, yy, mm, dd) {
+    return (((cc / 4) - 2 * cc - 1) + ((5 * yy / 4)) + ((26 * (mm + 1) / 10)) + dd) % 7
 }
 
 function myFunction() {
@@ -13,3 +13,5 @@ var cc = parseInt(prompt("Enter the century digits.For example 1989 has cc=19"))
 var yy = parseInt(prompt("Enter Year digits i.e 1989 has yy=89"))
 var mm = parseInt(prompt("Enter the month"))
 var dd = parseInt(prompt("Enter the Day of the month"))
+var result = d(cc, yy, mm, dd)
+alert(result)
