b8ed9629fc9b9390ee0b881cfd876432ffe29512
andrew <ayietaphilip00@outlook.com>
2020-07-18 20:57:27 +0300

    Add an alert that will warn the user to input the correct data

--- js/script.js
+++ js/script.js
@@ -2,13 +2,13 @@
 //var female = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]
 //var d = function (cc, yy, mm, dd) {
     //return (((cc / 4) - 2 * cc - 1) + ((5 * yy / 4)) + ((26 * (mm + 1) / 10)) + dd) % 7
-}
+//}
 
 //function myFunction() {
   //  var x = document.getElementById("myDate").value
     //document.getElementById("demo").innerHTML = x
 
-}
+//}
 //var cc = parseInt(prompt("Enter the century digits.For example 1989 has cc=19"))
 //var yy = parseInt(prompt("Enter Year digits i.e 1989 has yy=89"))
 //var mm = parseInt(prompt("Enter the month"))
@@ -124,3 +124,4 @@ function getGender(){
 function findName(){
   dayValue = calculateDayValue();
   getGender();
+}
